42f49303193c4024ab8648d65c7a1074f49d0b33
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-10-17 20:57:09 +0300

    :(((

--- 2-if_else_switch/IceCream/icecream.h
+++ 2-if_else_switch/IceCream/icecream.h
@@ -1,4 +1,15 @@
 int icecream(int x){
-    if((x%3 == 0 || x%5 == 0 || x%(3+5) == 0) && (x != 0)) return 1;
-    else return 0;
+    if (x == 0) return 0;
+    for (int i = 0; i <= (x/3); i++)
+    {
+        for (int j = 0; j <= (x/5); j++)
+        {
+            if (5*j + 3*i == x) 
+            {
+                return 1;
+            } 
+        }
+    }
+    return 0;
+
 }
