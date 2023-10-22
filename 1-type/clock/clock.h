2faeada1896849c35b4b6001e25f02b7578f7b2c
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-10-22 17:04:09 +0300

    Clock_2

--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -1,11 +1,11 @@
 int get_time(long long angle) {
     int ans;
     int hours, minutes, seconds;
-    hours = angle / (360*60);
-    angle = angle % (360*60);
+    hours = angle / (360 * 60);
+    angle = angle % (360 * 60);
     minutes = angle / 360;
     angle = angle % 360;
     seconds = angle/6;
-    ans = hours*10000 + minutes*100 + seconds;
+    ans = hours * 10000 + minutes * 100 + seconds;
     return ans;
-    }
+}
