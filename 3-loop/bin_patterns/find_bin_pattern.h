2545f1f541f728323bd64aeccd72b1e03258299c
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-10-23 18:11:11 +0300

    New

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,19 +1,19 @@
 #include <math.h>
 char find_bin_pattern(int number){
-    /*int arr[50];
-    for (int i = 0; i < 50; i++){
-        arr[i] = -10;
-    }
     int num = number;
+    int num_massive[50];
+    for (int k = 0; k < 50; k++)
+        num_massive[k] = -1;
     int i = 0;
-    while(num<0){
-        arr[i] = num%2;
-        num/=2;
+    while (num > 0){
+        num_massive[i] = num % 2;
+        num = num / 2;
         i++;
     }
-    int ans = 0;
-    for (int j = 0; j <= i; j){
-        if (arr[i])
-    }*/
-    return 100;
+    
+    int count = 0;
+    for (int j = 0; j < i-2;j++)
+        if ((num_massive[j]*100 + num_massive[j+1]*10 + num_massive[j+2]) == 101)
+            count+=1;
+    return count;
 }
