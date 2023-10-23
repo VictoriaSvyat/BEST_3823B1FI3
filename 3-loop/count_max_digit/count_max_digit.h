e25896108a1fd9a8b2b67556e072cc56764f3218
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-10-23 21:12:00 +0300

    New

--- 3-loop/count_max_digit/count_max_digit.h
+++ 3-loop/count_max_digit/count_max_digit.h
@@ -1,4 +1,22 @@
 #include <math.h>
 char count_max_digit(long long int x){
-    return 100;
+    long long help;    
+    int arr[10] = {};
+    for (int j = 0; j < 10; j++){
+        arr[j] = 0;
+    }
+    x = fabs(x);
+    while (x > 0){
+        help = x % 10;
+        arr[help] = arr[help] + 1;
+        x = x / 10;
+    }
+    int count = 0;
+    for (int j = 9; j >= 0; j--){
+        if (arr[j] > count){
+            count = arr[j];
+            break;
+        }   
+    }
+    return count;
 }
--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,5 +1,22 @@
 #include <math.h>
 
 double limit(double eps, int n_max){
-    return 0.0;
+    long double ans, ans1, minim;
+    if (n_max == 1) return 1 / sqrt(1*1+1);
+    else{
+        for (int i = 1; i<n_max; i++){
+            ans = i / sqrt(i*i+i);
+            ans1 = (i+1) / sqrt((i+1)*(i+1)+(i+1));
+            if ((ans1 - ans) <= eps){
+                minim = n_max / sqrt(n_max*n_max+n_max);
+                if (minim > ans1){
+                    minim = ans1;
+                    return minim;
+                } 
+                else return minim;
+            }
+                
+        }
+    }
+    
 }
