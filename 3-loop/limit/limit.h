04ac42a43e647f45c5f2eaeb72cf2057d883834d
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-10-26 23:44:44 +0300

    Newww

--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,22 +1,15 @@
 #include <math.h>
 
 double limit(double eps, int n_max){
-    long double ans, ans1, minim;
+    double ans, ans1, minim;
     if (n_max == 1) return 1 / sqrt(1*1+1);
     else{
-        for (int i = 1; i<n_max; i++){
+        for (int i = 0; i<n_max; i++){
             ans = i / sqrt(i*i+i);
             ans1 = (i+1) / sqrt((i+1)*(i+1)+(i+1));
-            if ((ans1 - ans) <= eps){
-                minim = n_max / sqrt(n_max*n_max+n_max);
-                if (minim > ans1){
-                    minim = ans1;
-                    return minim;
-                } 
-                else return minim;
-            }
-                
+            if ((ans1 - ans) <= eps)
+                    return ans1;
         }
+        return ans1;
     }
-    
 }
--- 3-loop/row/calc_row.h
+++ 3-loop/row/calc_row.h
@@ -1,3 +1,16 @@
 long long calc_row(int n, long long k){
+    unsigned long long ans = 0;
+    int flag = 0;
+    if (k > 0){
+        for (int i = 1; i <= n; i++)
+            ans += k*i;
+        return ans; 
+    }
+    else {
+        k = -k;
+        for (int i = 1; i <= n; i++)
+            ans += k*i;
+        return - ans;
+    }   
     return 0;
 }
