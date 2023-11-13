c4b7dc0eb3c616155f8692e337592a861fe1f3f7
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-11-13 20:00:17 +0300

    New

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -5,21 +5,27 @@
 int maxmin(int input[], int size){
     int array[15];
     for (int i = 0; i < 15; i++)
-        array[i] = -999999;
+        array[i] = -99999;
     int ans = 0;
-    int maximal;
-    if (input[0] < input[1])
-        maximal = input[0];
-    if (input[size-2] > input[size-1])
-        array[0] = input[size-1];
-    for (int i = 1; i < size-1; i++){
+    int maximal = -99999;
+    if (input[0] < input[1]){
+        array[0] = input[0];
+        ans++;
+    }
+        
+    if (input[size-2] > input[size-1]){
+        array[ans] = input[size-1];
+        ans++;
+    }
+    for (int i = 2; i < size-1; i++){
         if ((input[i-1] > input[i]) && (input[i] < input[i+1])){
-            ans++;
             array[ans] = input[i];
+            ans++;
         }
     }
+
     for (int i = 0; i <= ans; i++)
-        if (input[i]  < maximal)
-            maximal = input[i]; 
+        if ((array[i]  >  maximal) && (array[i] != -99999))
+            maximal = array[i];
     return maximal;
 }
