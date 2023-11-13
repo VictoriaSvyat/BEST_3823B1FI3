e844456abe0888f4d22ccadcb6be71b31fcd4968
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-11-13 20:03:50 +0300

    New

--- 4-array/more_on_left/left_more.h
+++ 4-array/more_on_left/left_more.h
@@ -1,6 +1,6 @@
 char left_more(double input[], unsigned int size){
     int ans = 0;
-    for (int i = size-1; i >= 0; i--)
-        if (input[i] < input[i+1]) ans++;
+    for (int i = 1; i <size; i++)
+        if (input[i] > input[i-1]) ans++;
     return ans;
 }
